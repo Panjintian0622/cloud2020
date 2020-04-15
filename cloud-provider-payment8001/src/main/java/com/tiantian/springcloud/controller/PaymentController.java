a17f7c8439cb958359736880cece5c07d04238d9
panjintian <panjintian1992@163.com>
2020-04-15 21:46:53 +0800

    eureka关闭自我保护机制

--- cloud-provider-payment8001/src/main/java/com/tiantian/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/tiantian/springcloud/controller/PaymentController.java
@@ -5,15 +5,20 @@ import com.tiantian.springcloud.entity.Payment;
 import com.tiantian.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
 public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
+    @Resource
+    private DiscoveryClient discoveryClient;
     @Resource
     private PaymentService paymentService;
 
@@ -36,5 +41,18 @@ public class PaymentController {
         }else{
             return new CommonResult(444,"没有对应记录，查询ID"+id,null);
         }
+    }
+
+    @GetMapping(value="/payment/discovery")
+    public Object discovery(){
+        List<String>  services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("*****element"+service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PROVIDER-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
